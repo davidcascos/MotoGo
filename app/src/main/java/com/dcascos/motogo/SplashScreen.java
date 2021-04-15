72c1d782fcd85723c904e97cfbf93d8ec2e72d7a
dcascos <davidcascosgimeno@gmail.com>
2021-04-15 23:00:24 +0200

    SplashScreen modified by adding animations

--- app/src/main/java/com/dcascos/motogo/SplashScreen.java
+++ app/src/main/java/com/dcascos/motogo/SplashScreen.java
@@ -1,14 +1,28 @@
 package com.dcascos.motogo;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
+
+	private static final int SPLASH_SCREEN_TIME = 5000;
+
+	//Variables
+	Animation topAnim, bottomAnim;
+	ImageView ivLogo;
+	TextView tvTitle;
 
 	@Override
 	@SuppressWarnings("DEPRECATION")
@@ -16,6 +30,12 @@ public class SplashScreen extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ac_splash_screen);
 
+		checkVersion();
+		doAnimations();
+		timerSplashScreen();
+	}
+
+	private void checkVersion() {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
 			final WindowInsetsController insetsController = getWindow().getInsetsController();
 			if (insetsController != null) {
@@ -24,5 +44,24 @@ public class SplashScreen extends AppCompatActivity {
 		} else {
 			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		}
+	}
+
+	private void doAnimations() {
+		topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+		bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+		ivLogo = findViewById(R.id.iv_logo);
+		tvTitle = findViewById(R.id.tv_title);
+
+		ivLogo.setAnimation(topAnim);
+		tvTitle.setAnimation(bottomAnim);
+	}
+
+	private void timerSplashScreen() {
+		new Handler(Looper.getMainLooper()).postDelayed(() -> {
+			Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+			startActivity(intent);
+			finish();
+		}, SPLASH_SCREEN_TIME);
 	}
 }
