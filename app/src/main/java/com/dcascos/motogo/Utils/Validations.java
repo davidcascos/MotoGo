a9c091b6e4146cef6ed9d11ddd51a3c202504d03
dcascos <davidcascosgimeno@gmail.com>
2021-04-16 19:08:25 +0200

    Created validations for forms

---
+++ app/src/main/java/com/dcascos/motogo/Utils/Validations.java
@@ -0,0 +1,78 @@
+package com.dcascos.motogo.Utils;
+
+import android.content.Context;
+
+import com.dcascos.motogo.Constants.Constants;
+import com.dcascos.motogo.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class Validations {
+
+	public static boolean validateFullNameFormat(Context context, TextInputLayout textInputLayout) {
+		String value = textInputLayout.getEditText().getText().toString().trim();
+
+		if (value.isEmpty()) {
+			textInputLayout.setError(context.getString(R.string.fieldCanNotEmpty));
+			return false;
+		} else if (!value.matches(Constants.NAMEFORMAT)) {
+			textInputLayout.setError(context.getString(R.string.invalidFormat, context.getString(R.string.fullName)));
+			return false;
+		} else {
+			textInputLayout.setError(null);
+			textInputLayout.setErrorEnabled(false);
+			return true;
+		}
+	}
+
+	public static boolean validateUsernameFormat(Context context, TextInputLayout textInputLayout) {
+		String value = textInputLayout.getEditText().getText().toString().trim();
+
+		if (value.isEmpty()) {
+			textInputLayout.setError(context.getString(R.string.fieldCanNotEmpty));
+			return false;
+		} else if (value.length() > 15) {
+			textInputLayout.setError(context.getString(R.string.usernameToLarge));
+			return false;
+		} else if (!value.matches(Constants.USERNAMEFORMAT)) {
+			textInputLayout.setError(context.getString(R.string.invalidFormat, context.getString(R.string.username)));
+			return false;
+		} else {
+			textInputLayout.setError(null);
+			textInputLayout.setErrorEnabled(false);
+			return true;
+		}
+	}
+
+	public static boolean validateEmailFormat(Context context, TextInputLayout textInputLayout) {
+		String value = textInputLayout.getEditText().getText().toString().trim();
+
+		if (value.isEmpty()) {
+			textInputLayout.setError(context.getString(R.string.fieldCanNotEmpty));
+			return false;
+		} else if (!value.matches(Constants.EMAILFORMAT)) {
+			textInputLayout.setError(context.getString(R.string.invalidFormat, context.getString(R.string.email)));
+			return false;
+		} else {
+			textInputLayout.setError(null);
+			textInputLayout.setErrorEnabled(false);
+			return true;
+		}
+	}
+
+	public static boolean validatePasswordFormat(Context context, TextInputLayout textInputLayout) {
+		String value = textInputLayout.getEditText().getText().toString().trim();
+
+		if (value.isEmpty()) {
+			textInputLayout.setError(context.getString(R.string.fieldCanNotEmpty));
+			return false;
+		} else if (!value.matches(Constants.PASSWORDFORMAT)) {
+			textInputLayout.setError(context.getString(R.string.invalidFormat, context.getString(R.string.password)));
+			return false;
+		} else {
+			textInputLayout.setError(null);
+			textInputLayout.setErrorEnabled(false);
+			return true;
+		}
+	}
+
+}
