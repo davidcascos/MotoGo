8c5f1519aa52270559d35a12aabaa6e8df8110ea
dcascos <davidcascosgimeno@gmail.com>
2021-04-16 04:14:32 +0200

    SplashScreen modified by adding animations from login to signUp

--- app/src/main/java/com/dcascos/motogo/Layouts/SignUp.java
+++ app/src/main/java/com/dcascos/motogo/Layouts/SignUp.java
@@ -1,16 +1,34 @@
 package com.dcascos.motogo.Layouts;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dcascos.motogo.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignUp extends AppCompatActivity {
+
+	TextInputLayout ti_fullname, tiUsername, ti_email, tiPassword;
+
+	Button btGo, bt_backLogin;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ac_sign_up);
+
+		ti_fullname = findViewById(R.id.ti_fullname);
+		tiUsername = findViewById(R.id.ti_username);
+		ti_email = findViewById(R.id.ti_email);
+		tiPassword = findViewById(R.id.ti_password);
+
+		btGo = findViewById(R.id.bt_go);
+		bt_backLogin = findViewById(R.id.bt_backLogin);
+
+		bt_backLogin.setOnClickListener(v -> {
+			this.onBackPressed();
+		});
 	}
 }
