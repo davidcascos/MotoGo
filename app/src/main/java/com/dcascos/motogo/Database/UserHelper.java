78764dece662404f8937522011140561df9b178f
dcascos <davidcascosgimeno@gmail.com>
2021-04-20 00:14:23 +0200

    Implemented log out with firebase

---
+++ app/src/main/java/com/dcascos/motogo/Database/UserHelper.java
@@ -0,0 +1,40 @@
+package com.dcascos.motogo.Database;
+
+public class UserHelper {
+
+	String fullName, username, email;
+
+	public UserHelper() {
+	}
+
+	public UserHelper(String fullName, String username, String email) {
+		this.fullName = fullName;
+		this.username = username;
+		this.email = email;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
--- app/src/main/java/com/dcascos/motogo/Layouts/EmptyActivity.java
+++ app/src/main/java/com/dcascos/motogo/Layouts/EmptyActivity.java
@@ -1,16 +1,36 @@
 package com.dcascos.motogo.Layouts;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dcascos.motogo.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class EmptyActivity extends AppCompatActivity {
+
+	private Button btSignOut;
+	private FirebaseAuth mAuth;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ac_empty);
+
+		btSignOut = findViewById(R.id.bt_signOut);
+		mAuth = FirebaseAuth.getInstance();
+
+		btSignOut.setOnClickListener(v -> {
+
+			mAuth.signOut();
+
+			startActivity(new Intent(EmptyActivity.this, Login.class));
+			finish();
+
+		});
+
+
 	}
 }
