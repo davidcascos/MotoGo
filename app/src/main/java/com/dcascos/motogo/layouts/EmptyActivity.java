2538cc05914678d6dcfa26e0d2bed17a8c7fa7b7
dcascos <davidcascosgimeno@gmail.com>
2021-04-20 23:59:22 +0200

    Retrieve login user data

--- app/src/main/java/com/dcascos/motogo/layouts/EmptyActivity.java
+++ app/src/main/java/com/dcascos/motogo/layouts/EmptyActivity.java
@@ -3,17 +3,29 @@ package com.dcascos.motogo.layouts;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.dcascos.motogo.layouts.loginSignup.Login;
 import com.dcascos.motogo.R;
+import com.dcascos.motogo.layouts.loginSignup.Login;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class EmptyActivity extends AppCompatActivity {
 
 	private Button btSignOut;
+
+	private TextView tvFullName;
+	private TextView tvEmail;
+
 	private FirebaseAuth mAuth;
+	private FirebaseDatabase database;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +33,32 @@ public class EmptyActivity extends AppCompatActivity {
 		setContentView(R.layout.ac_empty);
 
 		btSignOut = findViewById(R.id.bt_signOut);
+
+		tvFullName = findViewById(R.id.tv_fullName);
+		tvEmail = findViewById(R.id.tv_email);
+
 		mAuth = FirebaseAuth.getInstance();
+		database = FirebaseDatabase.getInstance();
+		DatabaseReference reference = database.getReference("Users");
+
+		reference.child(mAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+			@Override
+			public void onDataChange(@NonNull DataSnapshot snapshot) {
+				if (snapshot.exists()) {
+					String fullName = snapshot.child("fullName").getValue().toString();
+					String email = snapshot.child("email").getValue().toString();
+
+					tvFullName.setText(fullName);
+					tvEmail.setText(email);
+				}
+			}
+
+			@Override
+			public void onCancelled(@NonNull DatabaseError error) {
+
+			}
+		});
+
 
 		btSignOut.setOnClickListener(v -> {
 
