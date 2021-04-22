a277824342304c68bda614aaebadf66298988099
dcascos <davidcascosgimeno@gmail.com>
2021-04-22 19:43:31 +0200

    implemented Bottom Navigation menu

---
+++ app/src/main/java/com/dcascos/motogo/layouts/Home.java
@@ -0,0 +1,54 @@
+package com.dcascos.motogo.layouts;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.dcascos.motogo.R;
+import com.dcascos.motogo.fragments.HomeFragment;
+import com.dcascos.motogo.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Home extends AppCompatActivity {
+
+	BottomNavigationView bottomNavigation;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ac_home);
+
+		bottomNavigation = findViewById(R.id.bottom_navigation);
+
+		bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+		openFragment(new ProfileFragment());
+	}
+
+	public void openFragment(Fragment fragment) {
+		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+		transaction.replace(R.id.container, fragment);
+		transaction.addToBackStack(null);
+		transaction.commit();
+	}
+
+	BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = item -> {
+		switch (item.getItemId()) {
+			case R.id.itemHome:
+				openFragment(new HomeFragment());
+				return true;
+			case R.id.itemItem2:
+				openFragment(new HomeFragment());
+				return true;
+			case R.id.itemItem3:
+				openFragment(new HomeFragment());
+				return true;
+			case R.id.itemProfile:
+				openFragment(new ProfileFragment());
+				return true;
+			default:
+				return false;
+		}
+	};
+}
