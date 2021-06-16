8effe83507c1af0aa1fb98fd80eca2e554a62097
dcascos <davidcascosgimeno@gmail.com>
2021-06-16 20:59:06 +0200

    **Now can see the profile of the user who has commented by clicking on the photo

--- app/src/main/java/com/dcascos/motogo/adapters/CommentAdapter.java
+++ app/src/main/java/com/dcascos/motogo/adapters/CommentAdapter.java
@@ -1,6 +1,7 @@
 package com.dcascos.motogo.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.dcascos.motogo.R;
 import com.dcascos.motogo.constants.Constants;
+import com.dcascos.motogo.layouts.profile.ProfileFromUser;
 import com.dcascos.motogo.models.database.Comment;
 import com.dcascos.motogo.providers.database.UsersProvider;
 import com.dcascos.motogo.utils.Generators;
@@ -52,6 +54,8 @@ public class CommentAdapter extends FirestoreRecyclerAdapter<Comment, CommentAda
 
 		holder.tvCreationDate.setText(Generators.dateFormater(comment.getCreationDate()));
 		getUserInfo(comment.getUserId(), holder);
+
+		holder.civProfile.setOnClickListener(v -> context.startActivity(new Intent(context, ProfileFromUser.class).putExtra("userId", comment.getUserId())));
 	}
 
 	@NonNull
